afd5283a4e3fd183894d749482dcb90aaf88c5db
mark tyneway <mark@purse.io>
2018-06-09 23:39:54 -0500

    Return responses

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ const assert = require('assert');
 const { randomBytes } = require('bcrypto');
 const bodyParser = require('body-parser');
 
-const PORT = parseInt(process.env.PORT, 10) || 8080;
+const log = (string) => {
+  if (process.env.LOGGER) {
+    console.log(string);
+  }
+};
 
 class KeyServer {
-  constructor() {
+  constructor(port) {
     this.app = express();
     this.app.use(bodyParser.json({ extended: true }));
+    this.port = port;
     this.store = {
       user: {},
       device: {}
@@ -41,7 +46,7 @@ class KeyServer {
   }
 
   start() {
-    this.app.listen(PORT, () => console.log(`listening on ${PORT}`));
+    this.app.listen(this.port, () => console.log(`listening on ${this.port}`));
   }
 
   getToken() {
@@ -60,7 +65,7 @@ class KeyServer {
   registerRoutes() {
     this.app.get('/', (req, res) => res.json({ message: 'success' }));
     this.app.get('/pubkey/:type/:id', this.getPubkey)
-    this.app.post('/pubkey/:type/:id', this.addPubkey)
+    this.app.post('/pubkey/:type/:id', (req, res) => this.addPubkey(req, res));
     // TODO: auth this endpoint
     this.app.get('/all', (req, res) => res.json({ data: this.store }))
   }
@@ -72,7 +77,7 @@ class KeyServer {
     }
   }
 
-  getPubkey(req, res) {
+  getPubkey(req, res, auth) {
     this.auth(req, res);
     const { params } = req;
     const { type, id } = params;
@@ -92,26 +97,29 @@ class KeyServer {
     const { type, id } = params;
     const { key } = body;
 
+    log(`type: ${type}, id: ${id}`)
     // user must send key query param
     if (key === undefined) {
-      res.status(400).json({ message: 'please send key query param' });
+      return res.status(400).json({ message: 'please send key query param' });
     }
     // send 404 not found instead...
     // assert known type
     if (!(type in this.store)) {
-      res.status(404).json({ message: 'not found' });
+      return res.status(404).json({ message: 'not found' });
     }
     // assert not already in
     if (id in this.store[type]) {
-      res.status(404).json({ message: 'not found' });
+      return res.status(404).json({ message: 'not found' });
     }
 
     this.store[type][id] = key;
+    log(`added ${type}.${id} = ${key}`);
 
-    res.json({ message: 'success' });
+    res.json({message:"success"});
   }
 }
 
 if (require.main) {
-  const server = new KeyServer();
+  const PORT = parseInt(process.env.PORT, 10) || 8080;
+  const server = new KeyServer(PORT);
 }
